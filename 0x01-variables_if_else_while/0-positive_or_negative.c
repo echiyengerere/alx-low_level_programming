a9af3c41798e8834837ef35ea2b190f6f993fea2
echiyengerere <ellenchiyenge@gmail.com>
2023-02-23 04:45:31 -0800

    positive negatize zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
-
-/** *main-Prints a random number and states whether it is possitive, negative or zero.*
- * Return: Always 0. */
+/**
+ * main - Prints a random number and states wheter it is positive, negative, or zero.
+ * Return: Always 0.
+ */ 
 int main(void)
 {
 	int n;
