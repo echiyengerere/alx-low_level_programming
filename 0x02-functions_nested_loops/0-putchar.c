802d9a72ee44070f14a912b59c9ebd4b124d0757
echiyengerere <ellenchiyenge@gmail.com>
2023-02-26 03:05:49 -0800

    main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - program that prints Holberton.
- *
- * Return: int
+ * main - Print _putchar
+ * Return: Always 0
  */
-
 int main(void)
 {
-	int n = 0;
-	char str_hbt[9] = "_putchar";
-
-	while (n < 9)
-	{
-		_putchar(str_hbt[n]);
-		n += 1;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
-
+}
